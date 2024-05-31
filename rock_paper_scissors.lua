4ede7aba3a9b841e569e801397f17fa6db6b3f65
caslu <lucas1.martin@epitech.eu>
2024-05-31 16:43:58 +0400

    [ADD: rock, paper, scissors game : all functionality

--- rock_paper_scissors.lua
+++ rock_paper_scissors.lua
@@ -1,9 +1,39 @@
 local choice = {"rock", "paper", "scissors"}
 
-function computerChoice()
+function GetComputerChoice()
+    math.randomseed(os.time())
     local index = math.random(1, 3)
-    print("computer choice:", choice[index])
     return choice[index]
 end
 
-computerChoice()
+GetComputerChoice()
+
+function WinOrLoose(PlayerChoice, ComputerChoice)
+    if PlayerChoice == ComputerChoice then
+        return "Equality"
+    elseif PlayerChoice == "paper" and ComputerChoice == "rock" or
+           PlayerChoice == "scissors" and ComputerChoice == "paper" or
+           PlayerChoice == "rock" and ComputerChoice == "scissors" then
+        return "VICTORY!!!"
+    else
+        return "LOOSE"
+    end 
+
+end
+
+while true do
+    print("Choose your weapon: rock, paper or scissors (tape quit to quit)")
+    local PlayerChoice = io.read()
+    if PlayerChoice == "quit" then
+        break;
+    end
+
+    if PlayerChoice ~= "rock" and PlayerChoice ~= "paper" and PlayerChoice ~= "scissors" then
+        print("weapon not found")
+    else 
+        local computer = GetComputerChoice();
+        print("Computer choice: ", computer)
+        local results = WinOrLoose(PlayerChoice, computer);
+        print(results)
+    end
+end
